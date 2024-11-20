8d045eb2c6836cfc99e7f022bfd7575e27954b38
mn4563210 <mn4563210@gmail.com>
2024-11-20 10:57:44 +0900

    ignore 추가

--- src/components/public/sample-2.tsx
+++ src/components/public/sample-2.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export const Sample2 = () =>{
     return(
-        <div className="sample02"><h1>sample 02</h1></div>
+        <div className="sample02">
+            <h1>sample 02</h1>
+            div.
+        </div>
     )
 }
