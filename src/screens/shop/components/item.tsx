34175b16bbb50a853539d74d5a5345b7a136234b
잇츠 미 <yuella1219@naver.com>
2024-12-04 17:18:51 +0900

    구매가능 아이템 화폐 아이콘 종류 추가

--- src/screens/shop/components/item.tsx
+++ src/screens/shop/components/item.tsx
@@ -41,7 +41,14 @@ export const Item = ({item, sendItemNm, type="default"} :itemProps) =>{
             <span className="name">{item.item_display_name}</span>
             {item.item_count > 1 ? (<span className="cnt">{item.item_count}개 묶음</span>) : null}
             {item.limit_value ? (<span className="limit">구매 가능 횟수 {item.limit_value}회</span>) : null}
-            <span className="price">{item.price[0].price_value} <span>{item.price[0].price_type}</span></span>
+            <span className="price">
+                {item.price[0].price_value} 
+                { item.price[0].price_type === '골드' ? (<span className="gold">{item.price[0].price_type}</span>) 
+                : item.price[0].price_type === '두카트' ? (<span className="ducat">{item.price[0].price_type}</span>)
+                : item.price[0].price_type === '금박 솔방울' ? (<span className="pinecone">{item.price[0].price_type}</span>)
+                : (<span className="adv-seal">{item.price[0].price_type}</span>)
+            }
+            </span>
             {item.item_option.map((val, idx) => (
                 <span className="options" key={idx}>
                     <span className="option-txt">{val.option_type}</span>
