576e23607c14580ad151404b2b572a35bbf8968d
잇츠 미 <yuella1219@naver.com>
2024-12-10 17:17:19 +0900

    자동 커밋 마렵네

--- src/screens/public/components/todo.tsx
+++ src/screens/public/components/todo.tsx
@@ -29,6 +29,7 @@ export const Todo = () =>{
                 <span>전역상태로 생성 완료</span>
             </p>
             <p className="complete">아이템 카탈로그에 화폐 종류 구분</p>
+            <p>구매한 목록(baggage) 구현</p>
             <p>무게, 슬롯
                 <span>이동수단 및 교역 규모 선택</span>
                 <span>장바구니가 있고</span>
