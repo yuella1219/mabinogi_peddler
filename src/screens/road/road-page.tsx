d3d0d46814a54f41666623b05d753c5cc67c11e2
잇츠 미 <yuella1219@naver.com>
2025-02-04 18:00:06 +0900

    add: 오늘의 교훈 [잠을 잘 자자]

--- src/screens/road/road-page.tsx
+++ src/screens/road/road-page.tsx
@@ -12,13 +12,14 @@ interface RoadPageProps {
     dd : () => void;
 }
 
-const DEFAULT_SPEED = 8; // 배경 디폴트 스피드
 
 export const RoadPage = () =>{
+    const DEFAULT_SPEED = 8; // 배경 디폴트 스피드
     const {setLoading} = useLoading(); // 로딩
     const {npcName} = useNpcName(); // 이동할 npc 이름 받아오기
     const navigate = useNavigate(); // 시간 종료 시 페이지 이동 기능
     const [arrive, setArrive]  = useState(false); // 페이지 이동 감지용
+    const [backSpeed, setBackSpeed] = useState(DEFAULT_SPEED); 
     const backSpeedRef = useRef(DEFAULT_SPEED);// useRef는 리랜더링을 유발하지 않는다. 이걸 이제 알았네
     const roadArea = useRef<HTMLDivElement>(null); // 이동맵
     const isHolding = useRef(false); // 동작 감지
@@ -30,22 +31,26 @@ export const RoadPage = () =>{
             if (!isHolding.current) {
                 isHolding.current = true;
                 backSpeedRef.current /= 2;
+                setBackSpeed(backSpeedRef.current)
             }
         };
         const handleKeyUp = (e: KeyboardEvent) => {
             isHolding.current = false;
-            backSpeedRef.current *= 2;            
+            backSpeedRef.current *= 2;      
+            setBackSpeed(backSpeedRef.current)      
         };
         // 마우스 온오프
         const handleMouseDown = (e: MouseEvent) => {
             if (!isHolding.current) {
                 isHolding.current = true;
                 backSpeedRef.current /= 2;
+                setBackSpeed(backSpeedRef.current)
             }
         };
         const handleMouseUp = (e: MouseEvent) => {
             isHolding.current = false;                
             backSpeedRef.current *= 2;
+            setBackSpeed(backSpeedRef.current)
         };
         // 이벤트
         document.addEventListener("keydown", handleKeyDown);
@@ -77,7 +82,8 @@ export const RoadPage = () =>{
 
     // animation-duration 받을 준비
     return(
-        <div className="road-wrap" ref={roadArea}>
+        <div className="road-wrap" ref={roadArea}
+        style={{ "--road-speed": `${backSpeed}s` } as React.CSSProperties}>
             <div className="player"></div>
             <RoadTimer speed={isHolding.current ? 1 : 0} arrive={getArrive}/>
         </div>
